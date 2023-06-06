3895d5f0a12e792af3188f28e066389fb0547e0e
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-06 14:05:27 +0200

    modify linters

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 const navBtn = document.getElementById('nav-btn');
 const navbar = document.getElementById('navbar');
 const navClose = document.getElementById('nav-close');
+
 // show nav
 navBtn.addEventListener('click', () => {
   navbar.classList.add('showNav');
 });
+
 // close nav
 navClose.addEventListener('click', () => {
   navbar.classList.remove('showNav');
 });
+
 //setup date
 const date = (document.getElementById('date').innerHTML =
   new Date().getFullYear());
